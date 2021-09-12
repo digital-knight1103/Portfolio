f187fb58ce6539b7b33cd770b58b84d1a9535fce
Paweł Bokszczanin <bokszczanin.p@gmail.com>
2021-09-12 13:29:55 +0200

    Add header with nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,39 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
+console.log('hello');
+
+
+const navMenu = document.querySelector('.nav__menu')
+const navToggle = document.querySelector('.nav__toggle')
+const navClose = document.querySelector('.nav__close')
 
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if(navToggle) {
+  navToggle.addEventListener('click', () => {
+    console.log('Test');
+    navMenu.classList.add('show-menu')
+  })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
+if(navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const removeMenuMobile = () => {
+  const navMenu = document.querySelector('.nav__menu')
+  navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(el => el.addEventListener('click', removeMenuMobile))
 
 /*==================== ACCORDION SKILLS ====================*/
 
