20fb24f2c8d4e2292ee07a603703911f174011ef
Paweł Bokszczanin <bokszczanin.p@gmail.com>
2021-09-19 13:03:18 +0200

    Add section skill and quali

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,10 +36,48 @@ const removeMenuMobile = () => {
 navLink.forEach(el => el.addEventListener('click', removeMenuMobile))
 
 /*==================== ACCORDION SKILLS ====================*/
+// const skillsContent = document.getElementsByClassName('.skills__content')
+// const skillsHeader = document.querySelectorAll('.skills__header')
+
+
+// function toggleSkills() {
+//   let itemClass = this.ParentNode.className
+
+//   for(let i = 0; i < skillsContent.length; i++) {
+//     skillsContent[i].className = 'skills__content skills__close'
+//   }
+
+//   if(itemClass === 'skills__content skills__close') {
+//     this.parentNode.className = 'skills__content skills__open'
+//   }
+// }
+
+// skillsHeader.forEach((el) => {
+//   el.addEventListener('click', toggleSkills)
+// })
 
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]')
+const tabContents = document.querySelector('[data-content]')
 
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.map((tabContent) => {
+      tabContent.classList.remove('qualification__active')
+    })
+
+    target.classList.add('qualification__active')
+
+    tabs.forEach((tab) =>  {
+      tab.classList.remove('qualification__active')
+    })
+
+    tab.classList.add('qualification__active')
+  })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
