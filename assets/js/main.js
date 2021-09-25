283e80746d7aa5e6c86aed232abccaaa270cdc6d
Paweł Bokszczanin <bokszczanin.p@gmail.com>
2021-09-25 09:43:30 +0200

    Add scrollTop

--- assets/js/main.js
+++ assets/js/main.js
@@ -130,6 +130,7 @@ const modalViews = document.querySelectorAll('.services__modal'),
 
 // modalBtns.forEach(btn => btn.addEventListener('click', modalShow))
 
+
 /*==================== PORTFOLIO SWIPER  ====================*/
 // code from demos https://swiperjs.com/ 
 
@@ -167,12 +168,86 @@ var swiperTestimonial = new Swiper(".testimonial__container", {
 });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sectionsActive = document.querySelectorAll('section[id]')
 
+function scrollActive() {
+  const scrollY = window.pageYOffset
+
+  sectionsActive.forEach(current => {
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+    } else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
+}
+
+window.addEventListener('click' , scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
+const scrollHeader = () => {
+  const nav = document.querySelector('header')
 
+  if(this.scrollY >= 100) {
+    nav.classList.add('scroll-header') 
+  } else {
+    nav.classList.remove('scroll-header')
+  }
+}
+
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/ 
 
 
+const scrollTop = () => {
+  const scrollTop = document.querySelector('.scrollTop')
+
+  if(window.scrollY >= 500) {
+    scrollTop.classList.add('show-scroll') 
+  } else {
+    scrollTop.classList.remove('show-scroll')
+  }
+}
+
+window.addEventListener('scroll', scrollTop)
+
 /*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+//We obtain the current theme that the interface has by valitading the dark theme class
+const getCurrentTheme = () => {
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+}
+const getCurrentIcon = () => {
+  themeButton.classList.contains(iconTheme) ? 'uil-sun' : 'uil-moon'
+}
+
+//We validate if the user previously chose a topic 
+if (selectedTheme) {
+  // if the validation if fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+//Activate / deactivate the theme manually with button
+themeButton.addEventListener('click', () => {
+  //add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+
+  //we save the theme and the corrent icon that the user chose
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
